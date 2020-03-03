ff4711715012c2abf6f57af5f485b08a513fbbb6
Jonathan Millett <jon@millett.net>
2020-03-03 10:05:24 -0300

    Updated to thor 1.0.1.

--- gemset.nix
+++ gemset.nix
@@ -1,10 +1,12 @@
 {
   thor = {
+    groups = ["default"];
+    platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1yhrnp9x8qcy5vc7g438amd5j9sw83ih7c30dr6g6slgw9zj3g29";
+      sha256 = "1xbhkmyhlxwzshaqa7swy2bx6vd64mm0wrr8g3jywvxy7hg0cwkm";
       type = "gem";
     };
-    version = "0.20.3";
+    version = "1.0.1";
   };
 }
